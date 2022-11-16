2c892451c9925c7a0192a9adf7a47106abbacc30
Brooklyn Kairupan <brokinignacio3@gmail.com>
2022-11-16 14:26:09 +0800

    first commit

---
+++ model/WisataModel.js
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize";
+import db from "../config/database.js";
+
+const { DataTypes } = Sequelize;
+
+const Wisata = db.define("Tempat", {
+    nama: DataTypes.STRING,
+    alamat: DataTypes.STRING,
+    deskripsi: DataTypes.STRING,
+    biayaMasuk: DataTypes.INTEGER,
+}, {
+    freezeTableName: true
+});
+
+export default Wisata;
+
+(async() => {
+    await db.sync();
+})();
